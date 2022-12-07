40b77a81bfb8bf7e6b7f6e31ce38ae5527da0cc3
gwon entermen <sirius0147@daum.net>
2022-12-07 21:37:37 +0900

    Implement Main entry point
    실행될 메인 로직을 작성하고 정렬 알고리즘 사용
    테스트는 실행 결과만 관측하고 있음.
    검증코드는 없음

--- fastcampus-java-oop/src/main/java/org/fastcampus/Main.java
+++ fastcampus-java-oop/src/main/java/org/fastcampus/Main.java
@@ -1,7 +1,26 @@
 package org.fastcampus;
 
+import org.fastcampus.logic.BubbleSort;
+import org.fastcampus.logic.JavaSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("Bubble Sort : "+sort.sort(
+                Arrays.asList(args)
+                )
+        );
+        JavaSort<String> javaSort = new JavaSort<>();
+
+        System.out.println("Java Sort : "+javaSort.sort(
+                Arrays.asList(args)
+                )
+        );
     }
 }
