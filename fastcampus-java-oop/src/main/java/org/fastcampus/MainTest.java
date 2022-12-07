a1206b7719ad433f14fc78598a1bf781991769bc
gwon entermen <sirius0147@daum.net>
2022-12-07 21:37:37 +0900

    Implement Main entry point
    실행될 메인 로직을 작성하고 정렬 알고리즘 사용

---
+++ fastcampus-java-oop/src/main/java/org/fastcampus/MainTest.java
@@ -0,0 +1,15 @@
+package org.fastcampus;
+
+import org.junit.jupiter.api.Test;;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //given
+        String[] args = {"3","2","5"};
+        //when & then
+        Main.main(args);
+        //then
+    }
+}
